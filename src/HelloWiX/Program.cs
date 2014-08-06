aeeee921e54094ce179d7153ac99212e85c8d552
Mark Miller <developmentalmadness@gmail.com>
2014-08-05 21:27:39 -0600

    initial commit

---
+++ src/HelloWiX/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWiX
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ExeConfigurationFileMap map = new ExeConfigurationFileMap();
+			map.ExeConfigFilename = Path.Combine(@"C:\ProgramData\developMENTALmadness", Path.GetFileName(Assembly.GetExecutingAssembly().Location) + ".config");
+
+			var config = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
+			
+			var name = config.AppSettings.Settings["FullName"].Value;
+			
+			Console.WriteLine("Hello, {0}! From Wix", name);
+			Console.WriteLine("Press [Enter] to exit...");
+			Console.ReadLine();
+		}
+	}
+}
